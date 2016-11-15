3f7b17a4276914af01a0caa56bf4369620b28586
Re-1 <noirriel@gmail.com>
2016-11-15 10:11:58 +0100

    Add files via upload

---
+++ projekt-dbs.sql
@@ -0,0 +1,31 @@
+/* 
+
+Název: Návrh IS
+Předmět: DBS - Databázové systémy
+Garant: Ing. Jiří Kříž, Ph.D.
+Vyučující: Ing. Jan Luhan, Ph.D., MSc
+Vypracovali: Radek Daněk, Monika Masteiková, Anna Shadrina
+
+*/
+
+-- Vytvoření Schématu
+
+
+
+-- Vytvoření Databáze
+
+
+
+-- Vytvoření Rolí
+
+
+
+-- Vytvoření Tabulek
+
+
+
+-- Data do tabulek
+
+
+
+-- Práce s tabulkami
